78db9e256bc7653fb3c60bc05c549293d9c7216b
sheetal <saraogisheetal5@gmail.com>
2016-05-09 18:14:10 +0530

    updating pagerduty tests

--- src/main/java/com/sheetal/controller/TweetsController.java
+++ src/main/java/com/sheetal/controller/TweetsController.java
@@ -22,7 +22,7 @@ public class TweetsController {
     @ResponseBody
     List<Tweet> getPerson() {
         Client bugsnag = new Client("a2ff9af328186ca863ceeb96fdf659f2");
-        bugsnag.notify(new ArithmeticException("Non-fatal-3412"));
+        bugsnag.notify(new ArithmeticException("Non-fatal-341ef2"));
         //Get the Spring Context
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("/beanconfig.xml");
         //Get the EmployeeDAO Bean
